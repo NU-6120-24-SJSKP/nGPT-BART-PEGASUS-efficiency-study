e0b7c26789639b2a7fe3eeeac8fdfd3cb619c20d
Sanjiv Joshi <joshi.sanj@northeastern.edu>
2024-12-12 13:47:31 -0500

    Handle params not having all values

--- bart/config.py
+++ bart/config.py
@@ -11,31 +11,33 @@ class TrainingConfig:
     WEIGHT_DECAY = 0.01
     EARLY_STOPPING_PATIENCE = 3
 
-def create_small_bart_config(params = None):
-    if params:
+def create_small_bart_config(params=None):
+    try:
         config = BartConfig(
-        vocab_size=params.vocab_size,
-        max_position_embeddings=params.pos_embedding,
-        d_model=params.d_model,
-        encoder_layers=params.encoder_layers,
-        decoder_layers=params.decoder_layers,
-        encoder_attention_heads=params.enc_attn_head,
-        decoder_attention_heads=params.dec_attn_head,
-        encoder_ffn_dim=params.enc_ffnn_dim,
-        decoder_ffn_dim=params.decc_ffnn_dim,
-        activation_function="gelu"
+            vocab_size=params['vocab_size'],
+            max_position_embeddings=params['pos_embedding'],
+            d_model=params['d_model'],
+            encoder_layers=params['encoder_layers'],
+            decoder_layers=params['decoder_layers'],
+            encoder_attention_heads=params['enc_attn_head'],
+            decoder_attention_heads=params['dec_attn_head'],
+            encoder_ffn_dim=params['enc_ffnn_dim'],
+            decoder_ffn_dim=params['decc_ffnn_dim'],
+            activation_function="gelu"
         )
-    else:
+    except AttributeError:
+        print("Complete set of parameters were not provided for BART \n"
+              "Look at parameters help using 'python main.py -h'. Using default values")
         config = BartConfig(
-        vocab_size=50265,
-        max_position_embeddings=512,
-        d_model=600,
-        encoder_layers=7,
-        decoder_layers=7,
-        encoder_attention_heads=8,
-        decoder_attention_heads=8,
-        encoder_ffn_dim=2304,
-        decoder_ffn_dim=2304,
-        activation_function="gelu"
-        )     
+            vocab_size=50265,
+            max_position_embeddings=512,
+            d_model=600,
+            encoder_layers=7,
+            decoder_layers=7,
+            encoder_attention_heads=8,
+            decoder_attention_heads=8,
+            encoder_ffn_dim=2304,
+            decoder_ffn_dim=2304,
+            activation_function="gelu"
+        )
     return config
